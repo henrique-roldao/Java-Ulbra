0b0e2f0b0be27038f907453a5225f9456330a913
henrique-roldao <henriqueroldao@rede.ulbra.br>
2020-08-07 22:40:23 -0300

    Start

---
+++ src/com/company/MoonCalc.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MoonCalc {
+
+    public static void main(String[] args) {
+
+        float pesoTerra, calcMoon;
+
+        pesoTerra = 68.5f;
+        calcMoon = pesoTerra * 0.17f;
+
+        System.out.println(calcMoon);
+        
+    }
+}
